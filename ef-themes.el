25632c1346a88f7778050db67791e238207654ca
Protesilaos Stavrou <info@protesilaos.com>
2022-08-14 13:47:21 +0300

    Refine git commit warning faces
    These are for stylistic problems with:
    
    1. The summary line exceeds the desired length.
    2. The seccond line is not empty.

--- ef-themes.el
+++ ef-themes.el
@@ -312,8 +312,8 @@ When called from Lisp, THEME is a symbol."
     `(git-commit-comment-heading ((,c :inherit (bold font-lock-comment-face))))
     `(git-commit-comment-file ((,c :inherit font-lock-comment-face :foreground ,name)))
     `(git-commit-keyword ((,c :foreground ,keyword)))
-    `(git-commit-nonempty-second-line ((,c :inherit error)))
-    `(git-commit-overlong-summary ((,c :inherit warning)))
+    `(git-commit-nonempty-second-line ((,c :background ,bg-dim :foreground ,err)))
+    `(git-commit-overlong-summary ((,c :background ,bg-dim :foreground ,warning)))
     `(git-commit-summary ((,c :inherit bold :foreground ,string)))
 ;;;; git-rebase
     `(git-rebase-comment-hash ((,c :inherit font-lock-comment-face :foreground ,constant)))
