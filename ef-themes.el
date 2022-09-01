03875a68d941931e6e29a8b584ff2bff16e1708a
Protesilaos Stavrou <info@protesilaos.com>
2022-09-01 10:19:44 +0300

    Add support for Lin (my package)
    It uses the tinted backgrounds that were introduced in commit 6ebb8a8.
    
    The exact colour values from commit 6ebb8a8 may still be refined, though
    this gives us something to work with.

--- ef-themes.el
+++ ef-themes.el
@@ -944,6 +944,19 @@ Helper function for `ef-themes-preview-colors'."
 ;;;; keycast
     `(keycast-command ((,c :inherit bold)))
     `(keycast-key ((,c :background ,bg-accent :foreground ,fg-accent)))
+;;;; lin
+    `(lin-blue ((,c :background ,bg-blue-subtle)))
+    `(lin-cyan ((,c :background ,bg-cyan-subtle)))
+    `(lin-green ((,c :background ,bg-green-subtle)))
+    `(lin-magenta ((,c :background ,bg-magenta-subtle)))
+    `(lin-red ((,c :background ,bg-red-subtle)))
+    `(lin-yellow ((,c :background ,bg-yellow-subtle)))
+    `(lin-blue-override-fg ((,c :background ,bg-blue-subtle :foreground ,fg-intense)))
+    `(lin-cyan-override-fg ((,c :background ,bg-cyan-subtle :foreground ,fg-intense)))
+    `(lin-green-override-fg ((,c :background ,bg-green-subtle :foreground ,fg-intense)))
+    `(lin-magenta-override-fg ((,c :background ,bg-magenta-subtle :foreground ,fg-intense)))
+    `(lin-red-override-fg ((,c :background ,bg-red-subtle :foreground ,fg-intense)))
+    `(lin-yellow-override-fg ((,c :background ,bg-yellow-subtle :foreground ,fg-intense)))
 ;;;; line numbers (display-line-numbers-mode and global variant)
     ;; We need to fall back to `default' otherwise line numbers do not
     ;; scale when using `text-scale-adjust'.
