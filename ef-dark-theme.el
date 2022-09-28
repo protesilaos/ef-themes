6f807d9daff945a4c25b9919759d90fefba34c25
Protesilaos Stavrou <info@protesilaos.com>
2022-09-28 19:00:38 +0300

    ef-dark-theme: increase brightness of bg-region
    This brings it closer to how other themes in the collection are.

--- ef-dark-theme.el
+++ ef-dark-theme.el
@@ -126,7 +126,7 @@
       (bg-hover      "#004a5f")
       (bg-hover-alt  "#551f5a")
       (bg-hl-line    "#002435")
-      (bg-region     "#25164a")
+      (bg-region     "#2a234a")
       (bg-paren      "#20577a")
       (bg-err        "#330d09") ; check with err
       (bg-warning    "#332600") ; check with warning
