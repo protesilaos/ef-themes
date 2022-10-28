c1b3d334f53b814d4a1052db7b43a7e13a6bade1
Protesilaos Stavrou <info@protesilaos.com>
2022-10-28 10:09:42 +0300

    Update to version 0.9.0

--- ef-themes.el
+++ ef-themes.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Ef-Themes Development <~protesilaos/ef-themes@lists.sr.ht>
 ;; URL: https://git.sr.ht/~protesilaos/ef-themes
 ;; Mailing-List: https://lists.sr.ht/~protesilaos/ef-themes
-;; Version: 0.8.0
+;; Version: 0.9.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: faces, theme, accessibility
 
