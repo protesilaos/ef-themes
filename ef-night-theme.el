9e7d2ee1592fffe2960189e1381d390122f4e4d0
Protesilaos Stavrou <info@protesilaos.com>
2022-11-27 15:13:15 +0200

    ef-night: refine diff colours

--- ef-night-theme.el
+++ ef-night-theme.el
@@ -97,13 +97,13 @@
       (bg-cyan-subtle     "#004065")
 
       ;; Diffs
-      (bg-added          "#002d1f")
-      (bg-added-faint    "#001f18")
-      (bg-added-refine   "#003b2f")
+      (bg-added          "#00371f")
+      (bg-added-faint    "#002918")
+      (bg-added-refine   "#004c2f")
 
-      (bg-changed        "#323200")
-      (bg-changed-faint  "#292000")
-      (bg-changed-refine "#484700")
+      (bg-changed        "#363300")
+      (bg-changed-faint  "#2a1f00")
+      (bg-changed-refine "#4a4a00")
 
       (bg-removed        "#450f1f")
       (bg-removed-faint  "#2f060f")
