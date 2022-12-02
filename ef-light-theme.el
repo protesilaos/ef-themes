84279bbcbe209388cb95e79c4c89091107f33333
Protesilaos Stavrou <info@protesilaos.com>
2022-12-02 04:45:36 +0200

    ef-light: tweak magenta-faint

--- ef-light-theme.el
+++ ef-light-theme.el
@@ -72,7 +72,7 @@
       (magenta         "#ba35af")
       (magenta-warmer  "#cf25aa")
       (magenta-cooler  "#6052cf")
-      (magenta-faint   "#bf3580")
+      (magenta-faint   "#af5a80")
       (cyan            "#1f6fbf")
       (cyan-warmer     "#3f6faf")
       (cyan-cooler     "#1f77bb")
