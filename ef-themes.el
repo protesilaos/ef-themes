8c81a6e44c497c9857a22c615b1904b746a9f088
Protesilaos Stavrou <info@protesilaos.com>
2022-12-10 09:23:53 +0200

    Make notmuch-message-summary-face stand out more

--- ef-themes.el
+++ ef-themes.el
@@ -1560,7 +1560,7 @@ Helper function for `ef-themes-preview-colors'."
     `(notmuch-crypto-signature-good-key ((,c :inherit success)))
     `(notmuch-crypto-signature-unknown ((,c :inherit warning)))
     `(notmuch-jump-key ((,c :inherit ef-themes-key-binding)))
-    `(notmuch-message-summary-face ((,c :inherit bold :background ,bg-dim)))
+    `(notmuch-message-summary-face ((,c :inherit bold :background ,bg-alt)))
     `(notmuch-search-count ((,c :foreground ,fg-dim)))
     `(notmuch-search-date ((,c :foreground ,date)))
     `(notmuch-search-flagged-face ((,c :foreground ,err)))
