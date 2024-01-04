f59bf76cb1c7e6713053f6b3fe54b5a5ae7d8c9a
Protesilaos Stavrou <info@protesilaos.com>
2024-01-04 14:16:33 +0200

    ef-symbiosis: tweak prose-table-formula mapping

--- ef-symbiosis-theme.el
+++ ef-symbiosis-theme.el
@@ -224,7 +224,7 @@
       (prose-metadata fg-dim)
       (prose-metadata-value fg-alt)
       (prose-table fg-alt)
-      (prose-table-formula err)
+      (prose-table-formula warning)
       (prose-tag magenta-faint)
       (prose-todo red)
       (prose-verbatim blue-warmer)
