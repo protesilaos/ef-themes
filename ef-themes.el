9000fd245ff7c5c9b8fdcdf3d3289d6c35b21bc8
Protesilaos Stavrou <info@protesilaos.com>
2024-04-27 11:36:21 +0300

    Remove the bold-italic from doom-modeline because it breaks some icons
    Thanks to Filippo Argiolas for reporting this bug issue 42:
    <https://github.com/protesilaos/ef-themes/issues/42>.

--- ef-themes.el
+++ ef-themes.el
@@ -1279,7 +1279,7 @@ text should not be underlined as well) yet still blend in."
     `(doom-modeline-evil-visual-state ((,c :inherit bold :foreground ,modeline-warning)))
     `(doom-modeline-info ((,c :inherit bold :foreground ,modeline-info)))
     `(doom-modeline-input-method (( )))
-    `(doom-modeline-lsp-error ((,c :inherit bold-italic)))
+    `(doom-modeline-lsp-error ((,c :inherit bold)))
     `(doom-modeline-lsp-running (( )))
     `(doom-modeline-lsp-success ((,c :inherit bold :foreground ,modeline-info)))
     `(doom-modeline-lsp-warning ((,c :inherit bold :foreground ,modeline-warning)))
@@ -1290,7 +1290,7 @@ text should not be underlined as well) yet still blend in."
     `(doom-modeline-repl-success ((,c :inherit bold :foreground ,modeline-info)))
     `(doom-modeline-repl-warning ((,c :inherit bold :foreground ,modeline-warning)))
     `(doom-modeline-time (( )))
-    `(doom-modeline-urgent ((,c :inherit bold-italic :foreground ,modeline-err)))
+    `(doom-modeline-urgent ((,c :inherit bold :foreground ,modeline-err)))
     `(doom-modeline-warning ((,c :inherit bold :foreground ,modeline-warning)))
 ;;;; ediff
     `(ediff-current-diff-A ((,c :background ,bg-removed :foreground ,fg-removed)))
