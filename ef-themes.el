b1333b703805a21ed6414386830cb5d1977475b7
Protesilaos Stavrou <info@protesilaos.com>
2024-11-16 20:30:40 +0200

    Update the ef-themes to version 1.9.0

--- ef-themes.el
+++ ef-themes.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/ef-themes
-;; Version: 1.8.0
+;; Version: 1.9.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: faces, theme, accessibility
 
