45da971e95f03215da398e2d95db1bb452092a69
Protesilaos Stavrou <info@protesilaos.com>
2025-07-12 07:47:17 +0300

    Add explicit support for the tab-bar-tab-highlight face
    In Emacs 31, the face has a different style than before. We want the
    old one.

--- ef-themes.el
+++ ef-themes.el
@@ -2342,6 +2342,7 @@ text should not be underlined as well) yet still blend in."
     `(tab-bar-tab-group-current ((,c :inherit bold :background ,bg-tab-current :box (:line-width -2 :color ,bg-tab-current) :foreground ,fg-alt)))
     `(tab-bar-tab-group-inactive ((,c :background ,bg-tab-bar :box (:line-width -2 :color ,bg-tab-bar) :foreground ,fg-alt)))
     `(tab-bar-tab ((,c :inherit bold :box (:line-width -2 :color ,bg-tab-current) :background ,bg-tab-current)))
+    `(tab-bar-tab-highlight ((,c :inherit highlight)))
     `(tab-bar-tab-inactive ((,c :box (:line-width -2 :color ,bg-tab-other) :background ,bg-tab-other)))
     `(tab-bar-tab-ungrouped ((,c :inherit tab-bar-tab-inactive)))
 ;;;; tab-line-mode
