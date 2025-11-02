1d07b17f4e56e42bc98db3a0036d41a79ae80d3e
Protesilaos Stavrou <info@protesilaos.com>
2025-11-02 09:00:07 +0200

    Update to the actual modus-themes version we depend on
    The other was just a placeholder while I was working on modus-themes 5.0.0-dev.

--- ef-themes.el
+++ ef-themes.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/ef-themes
 ;; Version: 1.11.0
-;; Package-Requires: ((emacs "28.1") (modus-themes "4.8.1"))
+;; Package-Requires: ((emacs "28.1") (modus-themes "5.0.0"))
 ;; Keywords: faces, theme, accessibility
 
 ;; This file is NOT part of GNU Emacs.
